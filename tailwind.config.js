2de59e7362f8cf0156bcc5db73f8ca15e028a853
mohamedgad00 <gad254530@gmail.com>
2024-08-09 21:19:18 +0300

    Apply DarkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 tailwind.config = {
   theme: {
-    gridTemplateColumns: {
-      'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
-    },
-    fontFamily: {
-      Outfit: [ "Outfit", "sans-serif"],
-      Ovo: ["Ovo", "serif"]
-    },
-    animation: {
-      spin_slow: 'spin 6s linear infinite'
-    },
-    colors: {
-      lightHover: '#fcf4ff',
-      darkHover: '#2a004a'
+    extend: {
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+      },
+      fontFamily: {
+        Outfit: [ "Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      },
+      animation: {
+        spin_slow: 'spin 6s linear infinite'
+      },
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001f'
+      },
+      boxShadow: {
+        'black': '1px_1px_0_#000',
+        'white': ''
+      }
     }
-  }
+  },
+  darkMode: 'selector'
 }
